d39889a0062e91d5f15b901652d809056140b3d1
John Warren <john@bluestreamacademy.com>
2023-07-06 17:29:46 +0100

    Dev

--- scripts/WalkingSim/InteractableHelper.js
+++ scripts/WalkingSim/InteractableHelper.js
@@ -1,10 +1,13 @@
+import { devInteractions } from "./Interactables/InteractableDevInteractions";
+
 const range = 20;
 
 export class InteractableHelper {
-    constructor(player, interactable,interactionInputManager) {
+    constructor(player, interactable, interactionInputManager) {
         this.player = player;
         this.interactable = interactable;
 
+        this.removeDevInteraction = interactionInputManager.devInteractionRegister.registerFunc(()=>{this.onDevInteraction()});
         this.removeInteractEvent = interactionInputManager.interactEventRegister.registerFunc(()=>{this.onInteract()},"note:" + interactable.icon);
         this.removeThoughtReadEvent = interactionInputManager.mindReadEventRegister.registerFunc(()=>{this.onMindRead()});
         this.removeInteractExitEvent = interactionInputManager.interactionExitEventRegister.registerFunc(()=>{this.onInteractionExit()});
@@ -16,25 +19,32 @@ export class InteractableHelper {
         this.interactable.onInteract()
     }
     onMindRead(){
-        if(!this.interactable.isInRange || this.interactable.deactivated ){
+        if(!this.interactable.isInRange || this.interactable.deactivated){
             return
         }
-        this.interactable.onThoughtRead()
+        this.interactable.onThoughtRead();
     }
+    onDevInteraction(){
+        devInteractions.onDevInteraction(this.interactable);
+    }
+
     onInteractionExit(){
-        this.interactable.onInteractExit()
+        this.interactable.onInteractExit();
     }
+    
     update() {
         if (this.player.pos.distance(this.interactable.pos) < range) {
             this.interactable.isInRange = true;
         } else {
             this.interactable.isInRange = false;
         }
+        
     }
 
     draw() {}
     
     destroy(){
+        this.removeDevInteraction();
         this.removeInteractEvent();
         this.removeThoughtReadEvent();
         this.removeInteractExitEvent();
