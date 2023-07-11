37b618aa50f0e0df948dfee97d6ab3d7b724cc1d
John Warren <john@bluestreamacademy.com>
2023-07-11 11:57:49 +0100

    message editor pt1

--- scripts/WalkingSim/InteractableDevInteractions.js
+++ scripts/WalkingSim/InteractableDevInteractions.js
@@ -1,24 +1,26 @@
+import TemplatedHtml from "../TemplatedHtml";
 import { MessageInteractable } from "./Interactables/MessageInteractable";
 
 const devInteractionCodes = {
-    Export:1,
-    MoveToggle:2,
-    Create:3
+    Export: 1,
+    MoveToggle: 2,
+    Create: 3,
+    EditMessages: 4
 };
-(function(){
+(function () {
     var hs = "";
-    for(var i in devInteractionCodes){
-        hs+="<option value='"+devInteractionCodes[i]+"'>";
-        hs+=devInteractionCodes[i]+": "+i;
-        hs+="</option>"
+    for (var i in devInteractionCodes) {
+        hs += "<option value='" + devInteractionCodes[i] + "'>";
+        hs += devInteractionCodes[i] + ": " + i;
+        hs += "</option>"
     }
     document.getElementById("devTask").innerHTML = hs;
 
 
-    document.addEventListener("keydown",(e)=>{
+    document.addEventListener("keydown", (e) => {
 
         var numbers = "0123456789".split("");
-        if(numbers.includes(e.key)){
+        if (numbers.includes(e.key)) {
             document.getElementById("devTask").value = e.key;
         }
     })
@@ -35,56 +37,68 @@ class DevInteractions {
         this.onlyOneCounter = 0;
     }
 
-    setup(world){
+    setup(world) {
         this.world = world;
     }
 
     onDevInteraction(interactable) {
-        if(!(new URLSearchParams(window.location.search)).get("dev")){
+        if (!(new URLSearchParams(window.location.search)).get("dev")) {
             return;
         }
 
         var code = +document.getElementById("devTask").value;
         var inAccessible = !(interactable.isInRange && !interactable.deactivated);
+
+        var onlyOne = (f) => {
+            if (this.onlyOneCounter == 0) {
+                this.onlyOneCounter++;
+                f();
+            }
+        };
+        var onInteractable = (f)=>{
+            if (inAccessible) { return; }
+            f();
+        }
+
         switch (code) {
             case devInteractionCodes.Export:
-                if(inAccessible){return;}
-                this.showJson(interactable);
-            break;
+                onInteractable(()=>{this.showJson(interactable)});
+                break;
             case devInteractionCodes.MoveToggle:
-                if(inAccessible){return;}
-                this.toggleMoving(interactable);
-                
-            break;
+                onInteractable(()=>{this.toggleMoving(interactable)});
+
+                break;
             case devInteractionCodes.Create:
-                if(this.onlyOneCounter == 0){
-                    this.onlyOneCounter++;
-                    this.createInteractable();
+                onlyOne(() => { this.createInteractable() });
+                break;
+            case devInteractionCodes.EditMessages:
+                if(interactable?.hasOwnProperty("messages")){
+                    onInteractable(() => { this.showMessageEditUserInterface(interactable);})
                 }
-            break;
+                break;
             default:
                 break;
         }
-    }   
-    toggleMoving(interactable){
+    }
+    toggleMoving(interactable) {
         this.isMoving = !this.isMoving;
-                
-        if(this.isMoving){
+
+        if (this.isMoving) {
             this.movingInteractable = interactable;
-        }else{
+        } else {
             this.movingInteractable = null;
         }
     }
 
-    update(player){
+    update(player) {
         this.onlyOneCounter = 0;
         this.player = player;
-        if(this.isMoving && this.movingInteractable){
+        if (this.isMoving && this.movingInteractable) {
             this.movingInteractable.updatePos(this.player.pos);
         }
     }
-    
-    createInteractable(){
+
+    createInteractable() {
         var playerPos = this.player.pos.clone();
         var dat = () => new MessageInteractable(
             {
@@ -92,7 +106,7 @@ class DevInteractions {
                 icon: "🆕",
                 pos: playerPos.round(),
             }
-            ,[
+            , [
                 "The text",
                 "goes here"
             ]
@@ -103,8 +117,8 @@ class DevInteractions {
 
     showJson(interactable) {
         //Actually export wow
-        
-        if (!interactable instanceof  MessageInteractable){
+
+        if (!interactable instanceof MessageInteractable) {
             document.getElementById("messageObjectOutputJson").textContent = "Can't export";
             return;
         }
@@ -116,7 +130,66 @@ class DevInteractions {
         document.getElementById("messageObjectOutputJson").textContent = JSON.stringify(obj);
 
     }
+    showMessageEditUserInterface(i) {
+        new EditMessagesUserInterface(i, document.getElementById("devshit"), () => { this.showJson(i) });
+    }
+}
 
+function insertAfter(referenceNode, newNode) {
+    referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+}
+
+class EditMessagesUserInterface {
+
+    constructor(messageInteractable, htmlParent, onSave) {
+        this.onSave = onSave;
+        this.i = messageInteractable;
+        this.element = new TemplatedHtml("devMessageEdit", htmlParent);
+        
+        messageInteractable.messages.forEach(i=>{
+            this.getNewRowElement(i);
+
+        });
+        this.element.getPart("showIcon").textContent = messageInteractable.goData.id;
+        this.element.getPart("btn-submit").addEventListener("click", () => { this.onSubmit(); });
+        this.element.getPart("btn-close").addEventListener("click", () => { this.destroy(); });
+    }
+    getNewRowElement(text) {
+        var newInputElement = new TemplatedHtml("devMessageEditRow");
+        newInputElement.getPart("text").value = text;
+        newInputElement.getPart("btn-add").addEventListener("click", (e) => { this.addMessage(e) });
+        newInputElement.getPart("btn-remove").addEventListener("click", (e) => { this.removeMessage(e) });
+        this.element.getPart("messages").appendChild(newInputElement.element);
+        return newInputElement;
+    }
+    addMessage(e) {
+        var newInputElement = this.getNewRowElement();
+        var caller = e.target.closest(".devMessageEditRow");
+        insertAfter(caller, newInputElement.element);
+    }
+
+    removeMessage(e) {
+        if (newInputElement.querySelectorAll(".devMessageEditRow").length == 1) {
+            return;
+        }
+        var caller = e.target.closest(".devMessageEditRow");
+        caller.remove();
+    }
+    getMessageData() {
+        var messages = this.element.element.childNodes().map(i => i.getElementsByTagName("input").value);
+        return messages;
+    }
+    onSubmit() {
+        var messages = getMessageData;
+        messageInteractable.messages = messages;
+        messages.state.messageIndex = 0;
+        this.onSave();
+        this.destroy();
+    }
+
+    destroy() {
+        this.element.element.remove();
+    }
 }
 
 
